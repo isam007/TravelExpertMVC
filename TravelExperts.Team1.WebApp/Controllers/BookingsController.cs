b7197b2af53f47d1ee8d9fbaf010804b1ea59076
Irene Sam <irene.sam007@gmail.com>
2020-09-03 12:14:32 -0600

    Initial Commit

---
+++ TravelExperts.Team1.WebApp/Controllers/BookingsController.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Claims;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Xml.Schema;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using TravelExperts.Team1.WebApp.Managers;
+
+using TravelExperts.Team1.WebApp.Models;
+
+namespace TravelExperts.Team1.WebApp.Controllers
+{
+    public class BookingsController : Controller
+    {
+        //Marlon Rodriguez
+        // Added authorization anotation to limit access to booking details
+        // to authorized users only
+
+        [Authorize]
+        public ActionResult Index()
+        {
+
+            // Marlon Rodriguez
+            //  Called method to get CustomerId from Customers table
+
+            int userId = BookingManager.GetUserId(User.Identity.Name);
+            
+            var customerBookings = BookingManager.GetAllByBCustomerId(userId);
+            var viewModels = customerBookings.Select(a => new BookingsViewModel
+            {
+                BookingId = Convert.ToInt32(a.BookingId),
+                CustomerId = Convert.ToInt32(a.Booking.CustomerId),
+              
+                ItineraryNo =a.ItineraryNo,
+                TripStart = a.TripStart,
+                TripEnd = a.TripEnd,
+                Destination = a.Destination,
+                BasePrice = a.BasePrice,
+                AgencyCommission=a.AgencyCommission,
+                Description=a.Description,
+                TotalPrice= a.BasePrice + a.AgencyCommission
+
+            }).ToList();
+
+
+            // Marlon Rodriguez
+            //  Get bookings total
+
+            decimal? total = customerBookings.Select(t => t.BasePrice + t.AgencyCommission).Sum();
+
+            ViewBag.bookingsTotal = $"{total:N0}";
+
+
+
+            return View(viewModels);
+        }
+
+
+        
+    } 
+    }
---
+++ TravelExperts.Team1.WebApp/Managers/BookingManager.cs
@@ -0,0 +1,56 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using TravelExperts.Team1.WebApp.Models;
+
+namespace TravelExperts.Team1.WebApp.Managers
+{
+    public class BookingManager
+    {
+        public static List<BookingDetails> GetAllBookingDetails()
+        {
+            var context = new TravelExpertsContext();
+            var listOfBookingDetails = context.BookingDetails.Include(b => b.Booking);
+            return listOfBookingDetails.ToList();
+        }
+
+        public static List<BookingDetails> GetAllByBookingId(int id)
+        {
+            var context = new TravelExpertsContext();
+            var filteredList = context.BookingDetails.
+                Where(bid => bid.BookingId == id).
+                Include(b => b.Booking).ToList();
+            return filteredList;
+        }
+
+        public static List<BookingDetails> GetAllByBCustomerId(int id)
+        {
+            var context = new TravelExpertsContext();
+            var listOfBookingDetails = context.BookingDetails.
+            Include(b => b.Booking).
+            Where(c => c.Booking.CustomerId == id);
+            return listOfBookingDetails.ToList();
+        }
+
+        // Marlon Rodriguez
+        //  Programmed method to get logged in user id from database
+        // Irada moved to BookingManager to be also used by Customer Controller
+        public static int GetUserId(string userName)
+        {
+            var context = new TravelExpertsContext();
+            int userId = context.Customers.Where(u => u.Username == userName).
+                                           Select(u => u.CustomerId).FirstOrDefault();
+            return userId;
+        }
+        // Irada - added a manager for FirstName grabber
+        public static string GetUserName(string userName)
+        {
+            var context = new TravelExpertsContext();
+            string userFirstName = context.Customers.Where(u => u.Username == userName).
+                                           Select(u => u.CustFirstName).FirstOrDefault();
+            return userFirstName;
+        }
+    }
+}
